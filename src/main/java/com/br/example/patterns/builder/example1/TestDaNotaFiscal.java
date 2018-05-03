2b75175bc9f80aa0fb9a83f8429b84e35e997681
Fernando Torresan de Oliveira <fernando.oliveira@matera.com>
2018-05-03 17:34:42 -0300

    Ajustes na classe que testa o builder

--- src/main/java/com/br/example/patterns/builder/example1/TestDaNotaFiscal.java
+++ src/main/java/com/br/example/patterns/builder/example1/TestDaNotaFiscal.java
@@ -1,17 +1,19 @@
 package com.br.example.patterns.builder.example1;
 
+import java.util.Calendar;
+
 public class TestDaNotaFiscal {
 	
 	public static void main(String[] args) {
 		
 		NotaFiscalBuilder builder = new NotaFiscalBuilder();
 		
-		NotaFiscal notaFiscal = builder.setRazaoSocial("Razão social testes")
-									   .setCnpj("123")
-									   .setItem(new ItemDaNota("Item 01", 200.0))
-									   .setItem(new ItemDaNota("Item 02", 350))
-									   .setObservacoes("Observação exemplo")
-									   .build();
-		
+		builder.setRazaoSocial("Razão social testes")
+			   .setCnpj("123")
+			   .setItem(new ItemDaNota("Item 01", 200.0))
+			   .setItem(new ItemDaNota("Item 02", 350))
+			   .setObservacoes("Observação exemplo")
+			   .setData(Calendar.getInstance())
+			   .build();
 	}
 }
